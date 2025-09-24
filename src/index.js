9742e00061e94fe521d1232e05f7e4f89de4e7e5
dhavalbathe <dhavalbathe@gmail.com>
2025-09-24 19:22:07 +0530

    v1 api routes setup done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const app = express();
 
 const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
 
 const setUpAndStartServer = () => {
-    
-    const app = express();
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
+    app.use('/api', apiRoutes);
+    
     app.listen(PORT, () => {
         console.log(`Server is running on PORT: ${PORT}`);
 
